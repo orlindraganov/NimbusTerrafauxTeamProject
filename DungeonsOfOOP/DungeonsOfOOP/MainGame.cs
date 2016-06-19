657bc58a8bd593753a714a4cb86689ea6f5379a8
orlindraganov <orlindraganov@gmail.com>
2016-06-19 17:41:03 +0300

    Done most of the Quiz skeleton, to be finalised //(6okaritetki-ala-bala) Added super-basic test in MainGame.cs.Main()    //(Mainata)

--- DungeonsOfOOP/DungeonsOfOOP/MainGame.cs
+++ DungeonsOfOOP/DungeonsOfOOP/MainGame.cs
@@ -7,22 +7,38 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using DungeonsOfOOP.Structures;
 using DungeonsOfOOP.Views;
+using DungeonsOfOOP.Models;
+
 
 namespace DungeonsOfOOP
 {
     class MainGame
     {
         public static Renderer Renderer = new Renderer();
-
+        
         static void Main(string[] args)
         {
+            int questionIndex = 0;
+
+            if (Quiz.AskQuestion(questionIndex))
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            //Draw images
             Renderer.SetConsoleProperties();
             Renderer.SetPictureAtPosition("doncho", new Position(10, 40));
             Renderer.SetPictureAtPosition("cuki", new Position(150, 40));
             Renderer.SetPictureAtPosition("marto", new Position(10, 200));
             Renderer.SetPictureAtPosition("koceto", new Position(150, 200));
+
         }
     }
 }
--- DungeonsOfOOP/DungeonsOfOOP/Models/Quiz.cs
+++ DungeonsOfOOP/DungeonsOfOOP/Models/Quiz.cs
@@ -1,9 +1,94 @@
-﻿namespace DungeonsOfOOP.Models
+﻿//NOT FULLY READY YET, PLS DON'T TOUCH EXCEPT AFTER PHONE COORD 0899 998 056
+
+
+namespace DungeonsOfOOP.Models
 {
-    public class Quiz
+    using System;
+    using System.Text;
+    using System.Xml;
+
+    public static class Quiz
     {
-        public string Question { get; set; }
-        public string Answer { get; set; }
-        public bool IsAnswerRight { get; set; }
+        //Orlin: Access modifiers?
+        private static int NormaliseQuestionIndex(int questionIndex, XmlNodeList questions)
+        {
+            var maxIndex = questions.Count - 1;
+            if (questionIndex > maxIndex)
+            {
+                questionIndex = questionIndex % maxIndex;
+            }
+            return questionIndex;
+        }
+
+        private static XmlNodeList GetQuestions(string address)
+        {
+            var reader = new XmlDocument();
+            reader.Load(address);
+            XmlNodeList questions = reader.GetElementsByTagName("question");
+            return questions;
+        }
+
+        private static int WriteQuestion(XmlNode question)
+        {
+            //Writes question + answers on console, returns correct answer index (1 based)
+            var toPrint = new StringBuilder();
+            var correctAnswer = -1;
+            toPrint.Append("Question: ");
+            toPrint.Append(question.Attributes["text"].Value);
+            toPrint.Append("\n\r");
+            for (int i = 0; i < question.ChildNodes.Count; i++)
+            {
+                toPrint.Append(i + 1);
+                toPrint.Append(") ");
+                toPrint.Append(question.ChildNodes[i].Attributes["text"].Value);
+                toPrint.Append("\n\r");
+                if (question.ChildNodes[i].Attributes["correct"].Value == "true")
+                {
+                    correctAnswer = i + 1;
+                }
+            }
+            toPrint.Append("Please enter just the number of the answer you think is correct!");
+            Console.WriteLine(toPrint.ToString());
+            return correctAnswer;
+        }
+
+        private static bool CheckAnswer(int correctAnswer, int answersCount)
+        {
+            var givenAnswer = -1;
+            var userInput = Console.ReadLine();
+            bool isInt = int.TryParse(userInput, out givenAnswer);
+            if (!isInt || givenAnswer > answersCount || givenAnswer < 1)
+            {
+                throw new ArgumentException("Invalid input.");
+            }
+            if (givenAnswer == correctAnswer)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool AskQuestion(int questionIndex)
+        {
+            var questions = GetQuestions(@"./../../DataStorage/Questions.xml");
+            questionIndex = NormaliseQuestionIndex(questionIndex, questions);
+            int correctAnswer = WriteQuestion(questions[questionIndex]);
+            bool isValidAnswer = false;
+            while (!isValidAnswer)
+            {
+                try
+                {
+                    return CheckAnswer(correctAnswer, questions[questionIndex].ChildNodes.Count);
+                }
+                catch (ArgumentException invalidInput)
+                {
+                    Console.WriteLine(invalidInput.Message);
+                }
+            }
+            //inaccessible due try-catch in while but incompilable without
+            return false;
+        }
     }
 }
